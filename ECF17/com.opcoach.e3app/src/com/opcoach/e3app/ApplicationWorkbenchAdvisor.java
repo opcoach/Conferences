b0958c5535ac21eeb958bfecb66d70fc750d1151
Patrik Suzzi <psuzzi@gmail.com>
2017-06-18 10:04:39 +0200

    Initial Contribution for ECF 2017
    Added the source code for the ECF 2017 Talk "How to use the E4 Model
    Editor for your modular RCP applications"

---
+++ ECF17/com.opcoach.e3app/src/com/opcoach/e3app/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,22 @@
+package com.opcoach.e3app;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.opcoach.e3app.perspective";
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
